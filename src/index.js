2f2a40683d1a98f7b35fd7109c475a7f6ba3f1db
Huy Giang <gnhuy91@gmail.com>
2018-12-04 17:06:36 +0700

    update

--- src/index.js
+++ src/index.js
@@ -15,10 +15,6 @@ const render = ({ store }) => {
   );
 };
 
-// this is entirely optional, but here we here we try to pull starting data
-// from our local cache. We're using a util called money-clip here that
-// will only return if the version number is a match and it's not
-// older than the specified maxAge.
 cache.getAll().then(initialData => {
   if (initialData) {
     console.log('starting with locally cache data:', initialData);
